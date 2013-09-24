c2f8d823b4a09199bfa6cb6c4fffda0de2e79057
maksbu <kislomy@yandex.ru>
2013-09-24 18:24:53 +0400

    from work github

---
+++ SandBox/SandBox/SwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SwitchViewController.h
+//  SandBox
+//
+//  Created by Mac on 24.09.13.
+//  Copyright (c) 2013 maksburo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchViewController : UIViewController
+
+@end
