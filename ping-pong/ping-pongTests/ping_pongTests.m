f596339e2a48c2d71a720fadb0c7394818ad7917
maksbu <kislomy@yandex.ru>
2013-11-12 00:55:54 +0400

    first commit without workspace settings

---
+++ ping-pong/ping-pongTests/ping_pongTests.m
@@ -0,0 +1,34 @@
+//
+//  ping_pongTests.m
+//  ping-pongTests
+//
+//  Created by Mac on 11.11.13.
+//  Copyright (c) 2013 maksburo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ping_pongTests : XCTestCase
+
+@end
+
+@implementation ping_pongTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
